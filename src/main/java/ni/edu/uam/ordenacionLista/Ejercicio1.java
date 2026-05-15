0eff20427b21470677f1d042601171ec37515abf
ReyRod1505 <reynoldsra@uamv.edu.ni>
2026-05-15 16:31:43 -0600

    feat: "Elaboracion de ejercicio 1 de ordenacionLista"

--- src/main/java/ni/edu/uam/ordenacionLista/Ejercicio1.java
+++ src/main/java/ni/edu/uam/ordenacionLista/Ejercicio1.java
@@ -1,4 +1,24 @@
 package ni.edu.uam.ordenacionLista;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(45);
+        numeros.add(133);
+        numeros.add(48);
+        numeros.add(21);
+        numeros.add(39);
+
+        Collections.sort(numeros);
+
+        System.out.println("Números ordenados de menor a mayor:");
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
 }

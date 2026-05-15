1e0ad7bad152365d335b8891eae4435742ad1faa
ReyRod1505 <reynoldsra@uamv.edu.ni>
2026-05-15 16:05:20 -0600

    feat: "Elaboracion de ejercicio 1 de arrayList"

--- src/main/java/ni/edu/uam/arraylist/Ejercicio1.java
+++ src/main/java/ni/edu/uam/arraylist/Ejercicio1.java
@@ -1,4 +1,21 @@
 package ni.edu.uam.arraylist;
+import java.util.ArrayList;
 
 public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(10);
+        numeros.add(40);
+        numeros.add(54);
+        numeros.add(24);
+        numeros.add(52);
+
+        System.out.println("Lista de números:");
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
 }

1a7ac749c871fac08e57e712f315d0c86826dfc4
Pinoso31 <jdespinozar@uamv.edu.ni>
2026-05-15 16:52:04 -0600

    Order lista ejercicio1 terminado

--- src/main/java/ni/edu/uam/ordenamientoLista/Ejercicio1.java
+++ src/main/java/ni/edu/uam/ordenamientoLista/Ejercicio1.java
@@ -1,4 +1,25 @@
 package ni.edu.uam.ordenamientoLista;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.JOptionPane;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(45);
+        numeros.add(12);
+        numeros.add(78);
+        numeros.add(3);
+        numeros.add(25);
+
+        Collections.sort(numeros);
+
+        JOptionPane.showMessageDialog(
+                null,
+                "Números ordenados:\n" + numeros
+        );
+    }
 }
